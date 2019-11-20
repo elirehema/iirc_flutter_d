1c8495dcf8c04cd6c9d99eeca3e37e9a646ad7fb
=elirehema <=rocksandystone@gmail.com>
2019-11-20 17:50:30 +0300

    Added Help Screen

---
+++ lib/items_widget/index.dart
@@ -0,0 +1,1 @@
+export './items_appbar_home.dart';
---
+++ lib/widgets/widget_custom_icon_button.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CustomIconButton extends StatefulWidget {
+  final VoidCallback onPressed;
+  final String tooltip;
+  final Widget icon;
+  final Color color, focusColor,highlightColor, hoverColor, splashColor, disabledColor;
+  final double iconsize;
+  final EdgeInsetsGeometry edgeInsetsGeometry;
+  final AlignmentGeometry alignmentGeometry;
+  final FocusNode focusNode;
+  final bool autofocus;
+
+  const CustomIconButton({
+    Key key,
+    this.onPressed,
+    this.tooltip,
+    this.icon,
+    this.focusColor,
+    this.hoverColor,
+    this.splashColor,
+    this.disabledColor,
+    this.iconsize,
+    this.edgeInsetsGeometry,
+    this.alignmentGeometry,
+    this.focusNode,
+    this.autofocus,
+    this.highlightColor,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _CustomIconButtonWidgetState();
+}
+
+class _CustomIconButtonWidgetState extends State<CustomIconButton> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      iconSize: widget.iconsize,
+      padding: widget.edgeInsetsGeometry,
+      alignment: widget.alignmentGeometry,
+      icon: widget.icon,
+
+      highlightColor: widget.highlightColor,
+      tooltip: widget.tooltip,
+      onPressed: widget.onPressed,
+      color: widget.color,
+      hoverColor: widget.hoverColor,
+      splashColor: widget.splashColor,
+      disabledColor: widget.disabledColor,
+      focusColor: widget.focusColor,
+
+    );
+  }
+}
