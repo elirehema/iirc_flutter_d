9f01ef0b4cc8aa42e10e064d42e0c5185ca32eeb
=elirehema <=rocksandystone@gmail.com>
2019-11-21 16:10:28 +0300

    Configured Chopper

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,9 +2,30 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:amala_statistics/screens/index.dart';
 import 'package:amala_statistics/screens/ChartScreens/index.dart';
+import 'package:provider/provider.dart';
+import 'package:logging/logging.dart';
+import 'api/post_api_service.dart';
 
 class Routes {
-   final routes = <String, WidgetBuilder>{
+   
+
+    Routes(){
+      _setupLoggging();
+      runApp(MyApp());
+    }
+
+    void _setupLoggging(){
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen((rec){
+      print('${rec.level.name}:${rec.time}:${rec.message}');
+    });
+  }
+
+    
+}
+
+class MyApp extends StatelessWidget{
+  final routes = <String, WidgetBuilder>{
       '/HomeScreen': (BuildContext context) => new  HomeScreen(),
       '/LoginScreen': (BuildContext context) => new  Login(),
       '/GaugeScreen': (BuildContext context) => new GroupedStackedWeightPatternBarChart.withSampleData(),
@@ -12,12 +33,18 @@ class Routes {
       '/payments': (BuildContext context) => new MyHomePage(title: 'Home Page',),
       '/help': (BuildContext context)=> new HelpScreen(),
     };
-
-    Routes(){
-      runApp(new MaterialApp(
-        title: '',
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+     builder: (_)=> PostApiService.create(),
+      dispose: (context, PostApiService service)=> service.client.dispose(),
+      child: MaterialApp(
         routes: routes,
-        home: new SplashScreen(),
-      ));
-    }
+        title: '',
+        home: SplashScreen(),
+      ),
+     
+    );
+  }
+  
 }
--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -4,3 +4,4 @@ export './screen_settings.dart';
 export './screen_splash.dart';
 export './screen_transactions.dart';
 export './screen_help.dart';
+export './screen_user_posts.dart';
